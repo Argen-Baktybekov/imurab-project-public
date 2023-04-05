bf2bca153363aedceaddbfffef096ae39b456362
Argen Baktybekov <baktybekov.argen96@gmail.com>
2023-04-05 17:12:48 +0600

    added some dependencies

--- src/main/java/com/attractorschool/imurab/ImurabApplication.java
+++ src/main/java/com/attractorschool/imurab/ImurabApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImurabProjectApplication {
+public class ImurabApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ImurabProjectApplication.class, args);
+        SpringApplication.run(ImurabApplication.class, args);
     }
 
 }
